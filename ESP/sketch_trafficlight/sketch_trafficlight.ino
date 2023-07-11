cd760f89bf5479074a5136d55a4b190352fe8dbc
Fabian2611 <fabian.schwarz2010@gmail.com>
2023-07-11 20:20:22 +0200

    ESP Trafficlight V2

--- ESP/sketch_trafficlight/sketch_trafficlight.ino
+++ ESP/sketch_trafficlight/sketch_trafficlight.ino
@@ -1,33 +1,58 @@
-int red = D5;
-int ylw = D7;
-int grn = D8;
-
+int red = D7;
+int ylw = D6;
+int grn = D5;
+int wht = D2;
+int btn = D4;
+int bstate = 0;
+int prevstate = 0;
+int pressed = 0;
+int t1 = 0;
+int t2 = 0;
+int delays[4];
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   pinMode(red, OUTPUT);
   pinMode(ylw, OUTPUT);
   pinMode(grn, OUTPUT);
+  pinMode(btn, INPUT_PULLUP);
+  pressed = 0;
+  t1 = 0;
 }
 
 void loop() {
+  while(pressed < 4){
+    bstate = digitalRead(btn);
+    
+    if (bstate != prevstate){
+      if(bstate == HIGH){
+        delays[pressed] = millis() - t1;
+        pressed++;
+        t1 = millis();
+      }
+    }
+    prevstate = bstate;
+  }
+  
   digitalWrite(red, HIGH);
   digitalWrite(ylw, LOW);
   digitalWrite(grn, LOW);
-  delay(5000);
+  delay(delays[0]);
 
   digitalWrite(red, HIGH);
   digitalWrite(ylw, HIGH);
   digitalWrite(grn, LOW);
-  delay(1000);
+  delay(delays[1]);
 
   digitalWrite(red, LOW);
   digitalWrite(ylw, LOW);
   digitalWrite(grn, HIGH);
-  delay(5000);
+  delay(delays[2]);
   
   digitalWrite(red, LOW);
   digitalWrite(ylw, HIGH);
   digitalWrite(grn, LOW);
-  delay(1000);
+  delay(delays[3]);  
+
 }
